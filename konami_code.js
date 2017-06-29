ad0aed3f5d0ae9ee04c7c8c34f666419986676aa
sochetanie <allagorik2901@gmail.com>
2017-06-29 13:35:37 -0400

    done

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
 function init() {
   // your code here
+
+var i = 0
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which)
+ 
+  if (key === code[i]) {
+    i++
+ 
+    if (i === code.length - 1) {
+      alert("YOU DID IT!")
+      i = 0
+    }
+
+  } else {
+    i = 0
+  }
+
+}
+  document.body.addEventListener('keydown', onKeyDownHandler)
 }
